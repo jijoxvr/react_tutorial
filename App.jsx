e04c99a35d21f7c819c641420647847bab78cfca
jijo <jijo@qburst.com>
2017-05-19 17:00:57 +0530

    Setting up react using webpack

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World jij!!!
+         </div>
+      );
+   }
+}
+
+export default App;
